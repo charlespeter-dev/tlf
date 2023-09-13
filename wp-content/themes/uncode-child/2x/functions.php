a7215ec7f869201bedbbc051ea107f72d77b38ef
rifhanrazali <rifhan.razali@2x.marketing>
2023-09-13 21:32:40 +0800

    faces of tlf single template & button styling change

--- wp-content/themes/uncode-child/2x/functions.php
+++ wp-content/themes/uncode-child/2x/functions.php
@@ -55,12 +55,14 @@ add_filter('acf/settings/load_json', function ($paths) {
 add_action('after_setup_theme', function () {
     add_theme_support('post-thumbnails');
     add_image_size('_2x-carousel-hero', 1920, 550, true);
+    add_image_size('_2x_small-banner-hero', 1920, 375, true);
     add_image_size('_2x-carousel-testimonials', 1920, 520, true);
     add_image_size('_2x_footer-callout-banner', 1920, 350, true);
     add_image_size('_2x-carousel-news', 350, 350, true);
     add_image_size('_2x-carousel-resources-callout', 540, 340, true);
     add_image_size('_2x-card-customers', 439, 295, true);
     add_image_size('_2x-card-faces-of-tlf-left-top', 440, 339, ['left', 'top']);
+    add_image_size('_2x_face-image-single', 540, 465, true);
     add_image_size('_2x-card-news', 200, 200, true);
 });
 
---
+++ wp-content/themes/uncode-child/2x/single-faces-of-tlf.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Template Name: v2 / About Us / Faces of TLF - Single
+ * Template Post Type: post
+ */
+
+if (is_admin())
+    return;
+
+global $post;
+
+$options = get_fields('options');
+
+$fields = get_fields($post->ID);
+
+extract($fields);
+
+get_header() ?>
+
+<div class="bootstrap-container">
+
+    <section class="hero-carousels tlf-faces-single">
+        <div class="row-container">
+            <div class="single-h-padding limit-width position-relative">
+
+                <img src="<?= wp_get_attachment_image_url($background_image['id'], '_2x_small-banner-hero') ?>" class="medium-height" alt="<?= $background_image['title'] ?>">
+
+                <div class="_2x-hero-content">
+                    <div class="row">
+                        <div class="col-lg-12 text-center">
+                            <div class="mb-0">
+                                <h1 class="mb-0">
+                                    <?= $main_heading ?>
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+ 
+    <section class="face-overview">
+        <div class="row-container">
+            <div class="single-h-padding limit-width">
+                <figure>
+                    <img src="<?= wp_get_attachment_image_url($face_image['id'], '_2x_face-image-single') ?>" class="face-img" alt="<?= $face_image['title'] ?>">
+                </figure>
+                <div class="face-content">
+                    <?= $face_content ?>
+                </div>
+                <div class="cta-button mt-5">
+                    <a class="btn btn-primary" href="<?= $back_button_link ?>"><?= $back_button_text ?></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php if (isset($options['footer_callout_banner']) && $options['footer_callout_banner']): ?>
+        <section class="footer-callout-banner">
+            <div class="row-container">
+                <div class="single-h-padding limit-width position-relative">
+                    <img src="<?= wp_get_attachment_image_url($options['footer_callout_banner']['background_image'], '_2x_footer-callout-banner') ?>"
+                        class="full-width" alt="">
+
+                    <div class="footer-callout-banner-content">
+                        <div class="main-heading mb-4">
+                            <?= $options['footer_callout_banner']['main_heading'] ?>
+                        </div>
+                        <div class="cta">
+                            <a class="btn btn-primary" href="<?= $options['footer_callout_banner']['cta']['url'] ?>">
+                                <?= $options['footer_callout_banner']['cta']['title'] ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php endif ?>
+
+</div>
+
+<?= get_footer() ?>
