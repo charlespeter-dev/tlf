00e4f91ab6f6090ab97b35488c3063912bd2c5d4
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2023-09-14 16:06:53 +0800

    Single Resource - Brochure is ready

--- wp-content/themes/uncode-child/2x/single-resources.php
+++ wp-content/themes/uncode-child/2x/single-resources.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Template Name: v2 / Resources / Subpage v1
+ * Template Name: v2 / Resources / Single Brochure
  * Template Post Type: resources
  */
 
- if (is_admin())
- return;
+if (is_admin())
+    return;
 
 global $post;
 
@@ -14,4 +14,65 @@ $options = get_fields('options');
 
 $fields = get_fields($post->ID);
 
-print_r($fields); exit;
+extract($fields);
+
+get_header() ?>
+
+<div class="bootstrap-container resources single-brochure">
+
+    <?php if ($pdf): ?>
+        <section class="py-5">
+            <div class="row-container">
+                <div class="single-h-padding limit-width">
+
+                    <div class="top-content">
+                        <?= $top_content ?>
+                    </div>
+
+                    <div class="text-center my-4">
+                        <a href="<?= $pdf['url'] ?>" target="_blank" class="btn btn-primary">
+                            <?= $pdf['title'] ?>
+                        </a>
+                    </div>
+
+                    <iframe src="<?= $pdf['url'] ?>" frameborder="0"></iframe>
+
+                    <div class="row mt-5 align-items-center">
+                        <div class="col-lg-6">
+                            <img src="<?= $bottom_content['left_thumbnail_image'] ?>" class="img-fluid" alt="">
+                        </div>
+                        <div class="col-lg-6">
+                            <?= $bottom_content['right_content'] ?>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    <?php endif ?>
+
+    <?php if (isset($options['footer_callout_banner']) && $options['footer_callout_banner']): ?>
+        <section class="footer-callout-banner">
+            <div class="row-container">
+                <div class="single-h-padding limit-width position-relative">
+                    <img src="<?= wp_get_attachment_image_url($options['footer_callout_banner']['background_image'], '_2x_footer-callout-banner') ?>"
+                        class="full-width" alt="">
+
+                    <div class="footer-callout-banner-content">
+                        <div class="main-heading mb-4">
+                            <?= $options['footer_callout_banner']['main_heading'] ?>
+                        </div>
+                        <div class="cta">
+                            <a class="btn btn-primary" href="<?= $options['footer_callout_banner']['cta']['url'] ?>">
+                                <?= $options['footer_callout_banner']['cta']['title'] ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php endif ?>
+
+</div>
+
+<?php get_footer() ?>
