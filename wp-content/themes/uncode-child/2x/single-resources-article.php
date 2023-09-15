1613a77e1960a959b6de46ee8cfec74acc5442ae
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2023-09-15 13:23:52 +0800

    Resource single article ready

---
+++ wp-content/themes/uncode-child/2x/single-resources-article.php
@@ -0,0 +1,184 @@
+<?php
+
+/**
+ * Template Name: v2 / Resources / Single Article
+ * Template Post Type: page, post
+ */
+
+if (is_admin())
+    return;
+
+global $post;
+
+$options = get_fields('options');
+
+$fields = get_fields($post->ID);
+
+extract($fields);
+
+/**
+ * related resources
+ */
+
+$related_resources = _2x_related_resources($post->ID);
+
+get_header() ?>
+
+<div class="bootstrap-container resources single-article">
+
+    <section class="hero-carousels">
+        <div class="row-container">
+            <div class="single-h-padding limit-width position-relative">
+                <img src="<?= wp_get_attachment_image_url($background_image, '_2x_small-banner-hero') ?>"
+                    class="full-width" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="main-content my-5">
+        <div class="row-container">
+            <div class="single-h-padding limit-width position-relative">
+                <div>
+                    <?= $main_content ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php if (isset($about_author) && $about_author): ?>
+        <section class="about-author my-5">
+            <div class="row-container">
+                <div class="single-h-padding limit-width position-relative">
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <h3 class="red">
+                                <?= $about_author['main_heading'] ?>
+                            </h3>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <img class="img-fluid mb-4"
+                                src="<?= wp_get_attachment_image_url($about_author['profile_picture'], '_2x-carousel-news') ?>"
+                                alt="">
+                        </div>
+                        <div class="col-lg-9">
+                            <div class="about-author-name">
+                                <h3 class="blue">
+                                    <?= $about_author['author_name'] ?>
+                                </h3>
+                            </div>
+                            <div class="about-author-description">
+                                <?= $about_author['author_description'] ?>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    <?php endif ?>
+
+    <?php if (isset($article_sources) && $article_sources): ?>
+        <section class="about-author my-5">
+            <div class="row-container">
+                <div class="single-h-padding limit-width position-relative">
+                    <div>
+                        <?= $article_sources ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php endif ?>
+
+    <?php if (!empty($related_resources)): ?>
+        <section class="related-resources pb-5">
+            <div class="row-container">
+                <div class="single-h-padding limit-width position-relative">
+
+                    <hr class="my-5">
+
+                    <div class="row mb-5">
+                        <div class="col-lg-6">
+                            <h2 class="blue">
+                                <?= $options['related_resources']['main_heading'] ?>
+                            </h2>
+                        </div>
+                        <div class="col-lg-6 show-more-top">
+                            <div>
+                                <a href="<?= $options['related_resources']['cta']['url'] ?>" class="red">
+                                    <?= $options['related_resources']['cta']['title'] ?> <i
+                                        class="fa fa-arrow-right2 t-icon-size-lg"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row row-cols-1 row-cols-md-3 gy-4">
+                        <?php foreach ($related_resources as $post_id => $item): ?>
+                            <div class="col">
+
+                                <a href="<?= $item['url'] ?>">
+                                    <div class="card h-100">
+                                        <img class="img-fluid"
+                                            src="<?= wp_get_attachment_image_url(get_post_thumbnail_id($post_id), '_2x-carousel-resources-callout') ?>"
+                                            alt="">
+                                        <div class="card-body position-relative wpk-box-brand">
+                                            <p class="category mb-3">
+                                                <?= implode(' / ', $item['category']) ?>
+                                            </p>
+                                            <p class="post-title">
+                                                <?= $item['title'] ?>
+                                            </p>
+                                        </div>
+
+                                    </div>
+                                </a>
+
+                            </div>
+                        <?php endforeach ?>
+                    </div>
+
+                    <div class="row mt-5">
+                        <div class="col show-more-bottom">
+                            <div>
+                                <a href="<?= $options['related_resources']['cta']['url'] ?>" class="red">
+                                    <?= $options['related_resources']['cta']['title'] ?> <i
+                                        class="fa fa-arrow-right2 t-icon-size-lg"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    <?php endif ?>
+
+    <?php if (isset($options['footer_callout_banner']) && $options['footer_callout_banner']): ?>
+        <section class="footer-callout-banner">
+            <div class="row-container">
+                <div class="single-h-padding limit-width position-relative">
+                    <img src="<?= wp_get_attachment_image_url($options['footer_callout_banner']['background_image'], '_2x_footer-callout-banner') ?>"
+                        class="full-width" alt="">
+
+                    <div class="footer-callout-banner-content">
+                        <div class="main-heading mb-4">
+                            <?= $options['footer_callout_banner']['main_heading'] ?>
+                        </div>
+                        <div class="cta">
+                            <a class="btn btn-primary" href="<?= $options['footer_callout_banner']['cta']['url'] ?>">
+                                <?= $options['footer_callout_banner']['cta']['title'] ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php endif ?>
+
+</div>
+
+<?php get_footer() ?>
