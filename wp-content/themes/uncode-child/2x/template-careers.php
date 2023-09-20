7b94ecf6bbbba3e39bb83e541222ee30c0893877
rifhanrazali <rifhan.razali@2x.marketing>
2023-09-20 12:17:50 +0800

    adding jobs section id for marops to do anchor

--- wp-content/themes/uncode-child/2x/template-careers.php
+++ wp-content/themes/uncode-child/2x/template-careers.php
@@ -134,7 +134,7 @@ get_header() ?>
     <?php endif ?>
 
     <?php if ($jobs_section): ?>
-    <section class="jobs-section">
+    <section id="jobs-section" class="jobs-section">
         <div class="row-container">
             <div class="single-h-padding limit-width">
                 <div class="heading text-center">
