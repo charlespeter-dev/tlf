1f10244a680d59e09fb8f5d2f0c64bcf42839b15
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2023-09-22 21:08:57 +0800

    Careers: Single: relocated filterJS/Isotope inclusions from functions.php into single.php to avoid JS error

--- wp-content/themes/uncode-child/2x/single-careers.php
+++ wp-content/themes/uncode-child/2x/single-careers.php
@@ -13,7 +13,15 @@ global $post;
 $options = get_fields('options');
 
 $fields = get_fields($post->ID);
+
 extract($fields);
+
+/**
+ * specific css/js
+ */
+
+wp_enqueue_script('isotope', get_template_directory_uri() . '/library/js/isotopeLayout.min.js', array('jquery'), true);
+wp_enqueue_script('isotope-init', get_stylesheet_directory_uri() . '/2x/assets/js/filters.js', array('jquery', 'isotope'), true);
 
 get_header() ?>
 
@@ -23,7 +31,7 @@ get_header() ?>
         <div class="row-container">
             <div class="single-h-padding limit-width position-relative">
 
-            <?php $image_data = wp_get_attachment_image_src( get_post_thumbnail_id(), '_2x_xs-banner' ); ?>
+                <?php $image_data = wp_get_attachment_image_src(get_post_thumbnail_id(), '_2x_xs-banner'); ?>
                 <?php $image_url = $image_data[0]; ?>
 
                 <img src="<?php echo $image_url = $image_url; ?>" class="xs-height" alt="">
@@ -49,23 +57,23 @@ get_header() ?>
                     <?= $job_mission_profile ?>
                 </div>
                 <div class="row mt-5 mx-0">
-                        <div class="col-12 col-lg-6 m-0 p-0 job-benefits">
-                            <?= $job_benefits ?>
-                        </div>
-                        <div class="form-wrapper col-12 col-lg-6">
-                            <div id="contact-form" class="form-area">
-                                <h3 class="form-title">
-                                    <?= $form_title ?>
-                                </h3>
-                                <p class="form-desc">
-                                    <?= $form_description ?>
-                                </p>
-                                <div class="form-embed">
-                                    <?= $application_form_embed ?>
-                                </div>
+                    <div class="col-12 col-lg-6 m-0 p-0 job-benefits">
+                        <?= $job_benefits ?>
+                    </div>
+                    <div class="form-wrapper col-12 col-lg-6">
+                        <div id="contact-form" class="form-area">
+                            <h3 class="form-title">
+                                <?= $form_title ?>
+                            </h3>
+                            <p class="form-desc">
+                                <?= $form_description ?>
+                            </p>
+                            <div class="form-embed">
+                                <?= $application_form_embed ?>
                             </div>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
     </section>
@@ -74,8 +82,12 @@ get_header() ?>
         <div class="row-container">
             <div class="single-h-padding limit-width">
                 <div class="heading text-center">
-                    <h3><?= $job_section_heading ?></h3>
-                    <p><?= $job_section_subheading ?></p>
+                    <h3>
+                        <?= $job_section_heading ?>
+                    </h3>
+                    <p>
+                        <?= $job_section_subheading ?>
+                    </p>
                 </div>
                 <?php include 'jobs-filter.php'; ?>
             </div>
