a2938e06d2297485df4c005f8d4d52e9b4776627
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2024-12-18 16:38:31 +0800

    Improve search filter

--- wp-content/themes/uncode-child/2x/template-resources.php
+++ wp-content/themes/uncode-child/2x/template-resources.php
@@ -29,14 +29,169 @@ if (isset($hero_carousel[0]['search_filter_id']) && $hero_carousel[0]['search_fi
  * resources
  */
 
-$resources_query = new WP_Query([
-    'search_filter_id' => $search_filter_id,
+$args = [
+    'post_type' => 'resources',
+    'order' => 'DESC',
+    'orderby' => 'date',
+    'status' => 'publish',
     'fields' => 'ids',
-]);
+    'posts_per_page' => -1,
+];
+
+$_sf_s = '';
+
+if (isset($_GET['_sf_s']) && $_GET['_sf_s']) {
+
+    $_sf_s = $_GET['_sf_s'];
+
+    $args['meta_query'] = [
+        'relation' => 'OR',
+        [
+            'key' => 'main_content',
+            'value' => $_sf_s,
+            'compare' => 'LIKE',
+        ],
+        [
+            'key' => 'top_content',
+            'value' => $_sf_s,
+            'compare' => 'LIKE',
+        ],
+        [
+            'key' => 'bottom_content',
+            'value' => $_sf_s,
+            'compare' => 'LIKE',
+        ],
+        [
+            'key' => 'bottom_content_right_content',
+            'value' => $_sf_s,
+            'compare' => 'LIKE',
+        ],
+        [
+            'key' => 'infographic_summary_right_content',
+            'value' => $_sf_s,
+            'compare' => 'LIKE',
+        ],
+        [
+            'key' => 'headline',
+            'value' => $_sf_s,
+            'compare' => 'LIKE',
+        ],
+        [
+            'key' => 'main_heading',
+            'value' => $_sf_s,
+            'compare' => 'LIKE',
+        ],
+        [
+            'key' => 'sub_heading',
+            'value' => $_sf_s,
+            'compare' => 'LIKE',
+        ],
+    ];
+
+    if (isset($_GET['_sft_resource_category']) && $_GET['_sft_resource_category']) {
+
+        $_sft_resource_category = $_GET['_sft_resource_category'];
+
+        if (strpos($_sft_resource_category, ',') !== false) {
+            $_sft_resource_category = explode(',', $_sft_resource_category);
+        }
+
+        $args['tax_query'] = [
+            'relation' => 'AND',
+            [
+                'taxonomy' => 'resource_category',
+                'field' => 'slug',
+                'terms' => $_sft_resource_category,
+            ]
+        ];
+    }
+}
+
+$resources_query = new WP_Query($args);
+
+$resources_ids = $resources_query->posts;
 
 wp_reset_query();
 
-$resources_ids = $resources_query->posts;
+// ---------------------------------------
+// search by title using 'LIKE' operator
+// ---------------------------------------
+
+if ($_sf_s) {
+    function title_filter($where, &$wp_query)
+    {
+        global $wpdb;
+        if ($search_term = $wp_query->get('search_title')) {
+            $where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql($wpdb->esc_like($search_term)) . '%\'';
+        }
+        return $where;
+    }
+
+    add_filter('posts_where', 'title_filter', 10, 2);
+
+    $args = [
+        'post_type' => 'resources',
+        'order' => 'DESC',
+        'orderby' => 'date',
+        'status' => 'publish',
+        'fields' => 'ids',
+        'posts_per_page' => -1,
+        'search_title' => $_sf_s,
+        'post__not_in' => $resources_ids,
+    ];
+
+    if (isset($_GET['_sft_resource_category']) && $_GET['_sft_resource_category']) {
+
+        $_sft_resource_category = $_GET['_sft_resource_category'];
+
+        if (strpos($_sft_resource_category, ',') !== false) {
+            $_sft_resource_category = explode(',', $_sft_resource_category);
+        }
+
+        $args['tax_query'] = [
+            'relation' => 'AND',
+            [
+                'taxonomy' => 'resource_category',
+                'field' => 'slug',
+                'terms' => $_sft_resource_category,
+            ]
+        ];
+    }
+
+    $by_title_query = new WP_Query($args);
+
+    remove_filter('posts_where', 'title_filter', 10);
+
+    $resources_ids = array_merge($resources_ids, $by_title_query->posts);
+
+    wp_reset_query();
+}
+
+// ---------------------------------------
+// search by taxonomies only
+// ---------------------------------------
+
+if (isset($_GET['_sft_resource_category']) && $_GET['_sft_resource_category'] && !$_sf_s) {
+
+    $_sft_resource_category = $_GET['_sft_resource_category'];
+
+    if (strpos($_sft_resource_category, ',') !== false) {
+        $_sft_resource_category = explode(',', $_sft_resource_category);
+    }
+
+    $args['tax_query'] = [
+        'relation' => 'AND',
+        [
+            'taxonomy' => 'resource_category',
+            'field' => 'slug',
+            'terms' => $_sft_resource_category,
+        ]
+    ];
+
+    $by_tax_query = new WP_Query($args);
+
+    $resources_ids = $by_tax_query->posts;
+}
 
 $resource_categories = [];
 
@@ -173,9 +328,27 @@ get_header() ?>
     </section>
 
     <?php if (isset($cards_ids)): ?>
-        <section class="cards-overview faces-of-tlf my-5">
+        <section class="cards-overview faces-of-tlf mt-4 mb-5">
             <div class="row-container">
                 <div class="single-h-padding limit-width">
+
+                    <?php if (!$cards_ids): ?>
+                        <div class="row">
+                            <div class="col">
+                                <div class="alert alert-warning p-0" role="alert">
+                                    No resources found.
+                                </div>
+                            </div>
+                        </div>
+                    <?php else: ?>
+                        <div class="row">
+                            <div class="col">
+                                <div class="alert alert-warning p-0" role="alert">
+                                    <?= count($cards_ids) ?> resources found.
+                                </div>
+                            </div>
+                        </div>
+                    <?php endif ?>
 
                     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5">
 
