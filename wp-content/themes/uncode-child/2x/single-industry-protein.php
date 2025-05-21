541025593ecc15f2693d5ee850437ab20258808f
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2025-05-21 15:38:57 +0800

    Added back button

--- wp-content/themes/uncode-child/2x/single-industry-protein.php
+++ wp-content/themes/uncode-child/2x/single-industry-protein.php
@@ -524,6 +524,25 @@ get_header() ?>
 
     <?php endif ?>
 
+    <section class="cta-container">
+        <div class="row-container">
+            <div class="single-h-padding limit-width">
+
+                <div class="cta-inner">
+                    <a class="btn btn-primary cursor-init" href="https://thelogicfactory.com/industry/food-beverage/">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-arrow-left" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
+                        </svg>
+                        <span>Back to Food &amp; Beverage</span>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
     <?php if (!empty($_2x_related_resources)): ?>
         <section class="related-resources py-5">
             <div class="row-container">
